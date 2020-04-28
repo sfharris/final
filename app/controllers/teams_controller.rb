605354bcab25e4aa5aa06f710bf2b008120ab8cb
sfharris <samharris5451@gmail.com>
2020-04-28 04:47:38 +0000

    2nd

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,22 @@
 class TeamsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @teams = Team.all
   end
 
+   def new
+    @team = Team.new
+  end
+
+ def create
+  current_user.teams.create(team_params)
+  redirect_to root_path
+end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :games, :championships)
+  end
 end
